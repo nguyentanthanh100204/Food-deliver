087f3846c0e5e0fd110f20bda2b5d90559c9233a
nguyentanthanh100204 <151933939+nguyentanthanh100204@users.noreply.github.com>
2025-11-13 14:22:33 +0700

    Add unit test category to CartSummary test

--- FoodOrderingSystem.Tests/Controllers/ShoppingCartController_CartSummary_Tests.cs
+++ FoodOrderingSystem.Tests/Controllers/ShoppingCartController_CartSummary_Tests.cs
@@ -3,7 +3,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Moq;
-
+using FoodOrderingSystem.Tests; // TestCategories
 using FoodOrderingSystem.Controllers;
 
 namespace FoodOrderingSystem.Tests.Controllers
@@ -21,6 +21,8 @@ namespace FoodOrderingSystem.Tests.Controllers
         }
 
         [TestMethod]
+        [TestCategory(TestCategories.Unit)]
+
         public void CartSummary_ReturnsPartial_CartSummary()
         {
             // Arrange
